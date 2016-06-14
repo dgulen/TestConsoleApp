788d79f4eb8dd0e0ab79cb961b0b7ac712ea3b57
Damla  GULEN <dgulen@netas.lab.nortel.com>
2016-06-14 11:07:32 +0300

    Iterative fibonacci number calculation is added with max_int limit.

--- Program.cs
+++ Program.cs
@@ -16,8 +16,14 @@ namespace TestConsoleApp
                 case 1:
                     Console.WriteLine("1. secenek secildi, fibonacci sayısı hesaplanacak");
                     Console.WriteLine("Fibonacci sayisi hesaplanacak sayiyi giriniz:");
+                    int Y;
                     var fib = Console.ReadLine();
-                    var fibSeries = FibonacciSeries(Convert.ToInt32(fib));
+                    while (!Int32.TryParse(fib, out Y) || (Convert.ToInt32(fib)>31))
+                    {
+                        Console.WriteLine("Not a valid number, try again. Enter a number less than 31.");
+                        fib = Console.ReadLine();
+                    }
+                    var fibSeries = FibonacciIterative(Convert.ToInt32(fib));
                     Console.WriteLine(fibSeries);
                     break;
                 case 2:
@@ -58,13 +64,27 @@ namespace TestConsoleApp
             Console.ReadLine();
 
         }
-
+        // iterative fibonacci number calculation
+        public static int FibonacciIterative(int n)
+        {
+            int a = 0;
+            int b = 1;
+            for (int i = 0; i < n; i++)
+            {
+                int temp = a;
+                a = b;
+                b = temp + b;
+            }
+            return a;
+        }
+        // recursive fibonacci number calculation
         public static int FibonacciSeries(int fib)
         {
             if (fib <= 2)
                 return 1;
             return FibonacciSeries(fib - 1) + FibonacciSeries(fib - 2);
         }
+        // print main menu on screen
         public static int DisplayMenu()
         {
             Console.WriteLine("Istenen islem icin menudeki ilgili sayiyi tuslayiniz.");
@@ -76,6 +96,7 @@ namespace TestConsoleApp
             var result = Console.ReadLine();
             return Convert.ToInt32(result);
         }
+        // print hourglass with * and in the given size
         private static void PrintHourGlass(int couter)
         {
             for (int row = 0; row < couter ; row++)
